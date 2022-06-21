ea93b9e082bd7916fe03aa0ee7e9c67763611943
ferz1ka <fhozanzoti@hotmail.com>
2022-06-21 16:46:36 -0300

    tailwind setup and project cleanups

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+
+// Components
+import { App } from './App'
+
+// Styles
+import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
